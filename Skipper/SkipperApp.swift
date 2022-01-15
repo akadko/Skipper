665015f641e45d3b57a174bce27fdbd074dbef30
Tagir Nurakhmetov <t.nurakhmetov@wsmgroup.ru>
2022-01-15 19:13:09 +0300

    Initial Commit

---
+++ Skipper/SkipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkipperApp.swift
+//  Skipper
+//
+//  Created by akadko Akadko on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SkipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
